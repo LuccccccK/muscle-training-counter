6c6ff16f569b72b1123c136a4cdce634265043fd
lucccccck <oo.at.sw.tt@gmail.com>
2022-07-07 16:49:01 +0900

    カウンター実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Hoge {
+  value: number
 }
 
+const App = () => {
+  return <Counter />
+}
+
+class Counter extends React.Component<{}, Hoge>
+{
+  constructor(props: any)
+  {
+    super(props);
+    this.state = {
+      value : 0
+    } as Hoge
+  }
+
+  //インクリメントする関数
+  onIncrement = () => {
+    //setStateでstateの値を更新します
+    this.setState({ value : this.state.value + 1});
+  }
+
+  //デクリメントする関数
+  onDecrement = () => {
+    //setStateでstateの値を更新します
+    this.setState({ value : this.state.value - 1});
+  }
+
+  render()
+  {
+    return (
+      <div>
+        <div>
+          カウント値：{this.state.value}
+        </div>
+        <div>
+          <button type="button" onClick={this.onIncrement}>+</button>
+          <button type="button" onClick={this.onDecrement}>-</button>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
