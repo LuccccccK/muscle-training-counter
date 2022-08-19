61209cb26f33e9af44fdcdea6e5b5c0b3488a9be
lucccccck <oo.at.sw.tt@gmail.com>
2022-08-19 15:07:45 +0900

    クラスコンポーネントを関数コンポーネントに切り替え

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
-import Axios from 'axios';
-
-// Import MUI
+import React, { useEffect, useState } from 'react';
+import Axios, { AxiosRequestConfig } from 'axios';
 import { Box, Stack, Button, ButtonGroup, Backdrop, CircularProgress, Grid } from '@mui/material'
-
-// Import Fullcalendar Module
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
-
-// Import Utility
 import dateFormat from '../../utility/date';
-
 import './Register.css';
+import { useSelector } from 'react-redux';
+import { IStore } from '../../redux/store';
 
 interface CounterResult {
   selectedDate: string
@@ -21,61 +16,62 @@ interface CounterResult {
   countSquat: number
 }
 
-// Counter Classで利用するState 定義
-interface CounterState extends CounterResult{
-  isOpenSpinner: boolean
+interface TrainingSetting {
+  name: string
+}
+
+interface Training extends TrainingSetting {
+  count: number
 }
 
 const makeInitailState = (d: string) => {
   return {
-    isOpenSpinner: true,
     selectedDate: d,
     countPushUp: 0,
     countAbdominalMuscles: 0,
     countSquat: 0,
-  } as CounterState
+  } as CounterResult
 }
 
 const Register = () => {
-  return <RegisterComponent></RegisterComponent>
-}
+  const [isOpenSpinner, setIsOpenSpinner] = useState(true);
+  const [state, setState] = useState({} as CounterResult)
+  const [trainings, setTrainings] = useState([] as Training[])
 
-class RegisterComponent extends React.Component<{}, CounterState>
-{
-  constructor(props: any)
-  {
-    super(props);
-    this.state = makeInitailState(dateFormat(new Date()));
+  const credential = useSelector((state: IStore) => state.credential.credential);
+  const config: AxiosRequestConfig = {
+    headers: {
+      "Authorization": credential
+    }
   }
 
-  componentDidMount()
-  {
+  // 初回レンダリング時に必要なデータを取得しセット
+  useEffect(() => {
     const currentDate = dateFormat(new Date());
-    Axios.get("https://mtc.haba.link/api/result?selectedDate=" + currentDate).then((response) => {
-      const appState = response.data ? response.data as CounterState : makeInitailState(currentDate);
-      appState.isOpenSpinner = false;
-      this.setState(appState);
-    });
-  }
+    const fetchResultData = async () => {
+      const result = await Axios.get("https://mtc.haba.link/api/result?selectedDate=" + currentDate);
+      const appState = result.data ? result.data as CounterResult : makeInitailState(currentDate);
+      setState(appState);
+    }
+    fetchResultData();
 
-  // LocalStorageに筋トレ結果を保存
-  save()
-  {
-    this.setState({ isOpenSpinner: true });
-    const body = {
-      selectedDate: this.state.selectedDate,
-      countAbdominalMuscles: this.state.countAbdominalMuscles,
-      countPushUp: this.state.countPushUp,
-      countSquat: this.state.countSquat
-    } as CounterResult
-    Axios.post("https://mtc.haba.link/api/result", body).then((response) => {
-      this.setState({ isOpenSpinner: false });
-    });
-  }
+    const fetchSettingData = async () => {
+      const setting = await Axios.get("http://localhost:3001/nest-api/setting", config)
+      const trainings: Training[] = []
+      setting.data.trainings.map((e: TrainingSetting) => {
+        trainings.push({
+          name: e.name
+        } as Training)
+      })
+      setTrainings(trainings);
+    }
+    fetchSettingData()
+    
+    setIsOpenSpinner(false);
+  }, [])
 
   // 日付切り替え処理
-  switchDate(d: DateClickArg)
-  {
+  const switchDate = (d: DateClickArg) => {
     const currentDate = new Date();
     if (currentDate < d.date) {
       alert("未来日は選択できません");
@@ -91,91 +87,70 @@ class RegisterComponent extends React.Component<{}, CounterState>
     });
     d.dayEl.classList.add("date-selected");
 
-    this.setState({ isOpenSpinner: true });
+    setIsOpenSpinner(true);
     Axios.get("https://mtc.haba.link/api/result?selectedDate=" + d.dateStr).then((response) => {
-      const appState = response.data ? response.data as CounterState : makeInitailState(d.dateStr);
-      appState.isOpenSpinner = false;
-      this.setState(appState);
+      const appState = response.data ? response.data as CounterResult : makeInitailState(d.dateStr);
+      setState(appState);
+      setIsOpenSpinner(false);
     });
   }
 
-  render()
-  {
-    return (
-      <Box sx={{ pb: 7 }}>
-        <Backdrop
-          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open={this.state.isOpenSpinner}
-        >
-          <CircularProgress color="inherit" />
-        </Backdrop>
-        <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]} // 日付単位での表示、日付に対するinteraction実現
-          initialView="dayGridMonth" 
-          contentHeight={ 450 }
-          dateClick={(arg) => this.switchDate(arg)}
-        />
-        <Grid container spacing={1} m={1} alignItems="center">
-          <Grid item xs={4}>
-            <Box sx={{ textAlign: 'right' }}>日付：</Box>
-          </Grid>
-          <Grid item xs={8}>
-            <Box>{this.state.selectedDate}</Box>
-          </Grid>
-          <Grid item xs={4}>
-            <Box sx={{ textAlign: 'right' }}>腕立て伏せ：</Box>
-          </Grid>
-          <Grid item xs={2}>
-            <Box>{this.state.countPushUp}</Box>
-          </Grid>
-          <Grid item xs={6}>
-            <ButtonGroup variant="contained" aria-label="outlined primary button group">
-              <Button 
-                onClick={() => this.setState({ countPushUp : this.state.countPushUp + 1})}
-              >+1</Button>
-              <Button 
-                onClick={() => this.setState({ countPushUp : this.state.countPushUp - 1})}
-              >-1</Button>
-            </ButtonGroup>
-          </Grid>
-          <Grid item xs={4}>
-            <Box sx={{ textAlign: 'right' }}>腹筋：</Box>
-          </Grid>
-          <Grid item xs={2}>
-            <Box>{this.state.countAbdominalMuscles}</Box>
-          </Grid>
-          <Grid item xs={6}>
-            <ButtonGroup variant="contained" aria-label="outlined primary button group">
-              <Button 
-                onClick={() => this.setState({ countAbdominalMuscles : this.state.countAbdominalMuscles + 1})}
-              >+1</Button>
-              <Button 
-                onClick={() => this.setState({ countAbdominalMuscles : this.state.countAbdominalMuscles - 1})}
-              >-1</Button>
-            </ButtonGroup>
-          </Grid>
-          <Grid item xs={4}>
-            <Box sx={{ textAlign: 'right' }}>スクワット：</Box>
-          </Grid>
-          <Grid item xs={2}>
-            <Box>{this.state.countSquat}</Box>
-          </Grid>
-          <Grid item xs={6}>
-            <ButtonGroup variant="contained" aria-label="outlined primary button group">
-              <Button 
-                onClick={() => this.setState({ countSquat : this.state.countSquat + 1})}
-              >+1</Button>
-              <Button 
-                onClick={() => this.setState({ countSquat : this.state.countSquat - 1})}
-              >-1</Button>
-            </ButtonGroup>
-          </Grid>
-        </Grid>
-        <Stack direction="column" spacing={1} m={2}>
-          <Button variant="contained" color="primary" onClick={() => this.save()}>Save</Button>
-        </Stack>
-      </Box>
-    );
+  // 筋トレ結果を保存
+  const save = () => {
+    setIsOpenSpinner(true);
+    const body = {
+      selectedDate: state.selectedDate,
+      countAbdominalMuscles: state.countAbdominalMuscles,
+      countPushUp: state.countPushUp,
+      countSquat: state.countSquat
+    } as CounterResult
+    Axios.post("https://mtc.haba.link/api/result", body).then((response) => {
+      setIsOpenSpinner(false);
+    });
   }
+
+  return (
+    <Box sx={{ pb: 7 }}>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isOpenSpinner}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]} // 日付単位での表示、日付に対するinteraction実現
+        initialView="dayGridMonth" 
+        contentHeight={ 450 }
+        dateClick={(arg) => switchDate(arg)}
+      />
+      <Grid container spacing={1} m={1} alignItems="center">
+        <Grid item xs={4}>
+          <Box sx={{ textAlign: 'right' }}>日付：</Box>
+        </Grid>
+        <Grid item xs={8}>
+          <Box>{state.selectedDate}</Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box sx={{ textAlign: 'right' }}>腕立て伏せ：</Box>
+        </Grid>
+        <Grid item xs={2}>
+          <Box>{state.countPushUp}</Box>
+        </Grid>
+        <Grid item xs={6}>
+          <ButtonGroup variant="contained" aria-label="outlined primary button group">
+            <Button 
+              onClick={() => console.log(state.countPushUp)}
+            >+1</Button>
+            <Button 
+              onClick={() => console.log(state.countPushUp)}
+            >-1</Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+      <Stack direction="column" spacing={1} m={2}>
+        <Button variant="contained" color="primary" onClick={() => save()}>Save</Button>
+      </Stack>
+    </Box>
+  );
 }
 export default Register;
